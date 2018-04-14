9c73d1507e4a9db2135f4950e02a329fe690bb06
Huascar <huascarmc@hotmail.com>
2018-04-14 15:01:24 +0100

    added database constraints for string fields

--- src/main/java/com/example/springdatasample/SpringDataSampleApplication.java
+++ src/main/java/com/example/springdatasample/SpringDataSampleApplication.java
@@ -47,7 +47,11 @@ class Book {
 	@Id
 	@GeneratedValue
 	private Long id;
+	@NotBlank
+	@Size(max=255)
 	private String title;
+	@NotBlank
+	@Size(max=255)
 	private String description;
 
 	Book(String title, String description) {
