ce05c7383264a3d1f14ba08d2d727cead661bbc7
Huascar <huascarmc@hotmail.com>
2018-04-14 11:53:56 +0100

    added book class with JPA annotations

--- src/main/java/com/example/springdatasample/SpringDataSampleApplication.java
+++ src/main/java/com/example/springdatasample/SpringDataSampleApplication.java
@@ -9,4 +9,22 @@ public class SpringDataSampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataSampleApplication.class, args);
 	}
+
+	@Entity // makes it a JPA entity.
+	@Data	// does all the boiler plate for the class, setters for non final
+	//fields/getters/toString/equals/hashCode/constructor that initializes all fields.
+	@NoArgsConstructor // generates constructor with no parameters, just for the sake of JPA.
+	class Book {
+	   @Id // JPA recognizes this as the objects ID.
+	   @GeneratedValue // to indicate that the ID should be generated automatically.
+		 // title and description are left unnanotated, it is assumed they will be mapped to columns
+		 // that share the same properties as the values themselves.
+	   private Long id;
+	   private String title;
+	   private String description;
+	   Book(String title, String description) {
+	      this.title = title;
+	      this.description = description;
+	   }
+	}
 }
