52501490584e16be990b70b31af7aaccbb1cd9ff
Huascar <huascarmc@hotmail.com>
2018-04-14 13:14:51 +0100

    build-succesful with crudrepository implementation

--- src/main/java/com/example/springdatasample/SpringDataSampleApplication.java
+++ src/main/java/com/example/springdatasample/SpringDataSampleApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import javax.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.repository.CrudRepository;
 
 @SpringBootApplication
 public class SpringDataSampleApplication {
@@ -14,17 +16,21 @@ public class SpringDataSampleApplication {
 	}
 }
 
-@Entity
-@Data
-@NoArgsConstructor
-class Book {
-   @Id
-   @GeneratedValue
-   private Long id;
-   private String title;
-   private String description;
-   Book(String title, String description) {
-      this.title = title;
-      this.description = description;
-   }
+	@Entity
+	@Data
+	@NoArgsConstructor
+	class Book {
+	   @Id
+	   @GeneratedValue
+	   private Long id;
+	   private String title;
+	   private String description;
+	   Book(String title, String description) {
+	      this.title = title;
+	      this.description = description;
+	 }
+
+  @RepositoryRestResource
+		interface  BookRepository extends CrudRepository {
+	}
 }
