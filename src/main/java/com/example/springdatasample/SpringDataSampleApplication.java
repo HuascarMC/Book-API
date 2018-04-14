935c11f874483c08f62a20ab9564597deb4d60c3
Huascar <huascarmc@hotmail.com>
2018-04-14 14:26:58 +0100

    build failing unknown reason

--- src/main/java/com/example/springdatasample/SpringDataSampleApplication.java
+++ src/main/java/com/example/springdatasample/SpringDataSampleApplication.java
@@ -5,11 +5,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import javax.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.repository.CrudRepository;
 
 @SpringBootApplication
 public class SpringDataSampleApplication {
+
+
+		@Bean
+		CommandLineRunner initData(BookRepository bookRepository)	{
+	   return args -> {
+	      bookRepository.save(new Book("Spring Microservices", "Learn how to efficiently build and implement microservices in Spring,\n" +
+	            "and how to use Docker and Mesos to push the boundaries. Examine a number of real-world use cases and hands-on code examples.\n" +
+	            "Distribute your microservices in a completely new way"));
+	      bookRepository.save(new Book("Pro Spring Boot", "A no-nonsense guide containing case studies and best practise for Spring Boot"));
+	   };
+		}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataSampleApplication.class, args);
@@ -28,9 +41,9 @@ public class SpringDataSampleApplication {
 	   Book(String title, String description) {
 	      this.title = title;
 	      this.description = description;
-	 }
-
-  @RepositoryRestResource
-		interface  BookRepository extends CrudRepository {
+	   }
 	}
-}
+
+	@RepositoryRestResource
+	interface  BookRepository extends CrudRepository {
+	}
