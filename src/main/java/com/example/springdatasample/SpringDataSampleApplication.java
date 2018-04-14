45b9de2abd7254c392d8366b6ca07e2e2d5ad924
Huascar <huascarmc@hotmail.com>
2018-04-14 11:27:08 +0100

    initial commit

---
+++ src/main/java/com/example/springdatasample/SpringDataSampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.springdatasample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataSampleApplication.class, args);
+	}
+}
